649ab11b302ec545ed007e4e12ac693c3d871082
dhanaprabhu <dhana.prabhu@continualengine.com>
2020-11-09 13:29:10 +0530

    Updates

---
+++ precache-manifest.332ee2ce5dc3c8ca113b6d72ab83d654.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9effb73c82e9507d2e6e3a050651c59",
+    "url": "/ce-admin/index.html"
+  },
+  {
+    "revision": "c3e8927908e8d23f580b",
+    "url": "/ce-admin/static/css/main.beee7a75.chunk.css"
+  },
+  {
+    "revision": "a539724a315cb5ce4451",
+    "url": "/ce-admin/static/js/2.36ba0325.chunk.js"
+  },
+  {
+    "revision": "9e0280b943c276a78b8884f29f51efcb",
+    "url": "/ce-admin/static/js/2.36ba0325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e8927908e8d23f580b",
+    "url": "/ce-admin/static/js/main.3b28bc07.chunk.js"
+  },
+  {
+    "revision": "d007fddc4b515f8783c5",
+    "url": "/ce-admin/static/js/runtime-main.08b984eb.js"
+  },
+  {
+    "revision": "6b4972ad19f907d12e970936f65fd02b",
+    "url": "/ce-admin/static/media/1.6b4972ad.png"
+  },
+  {
+    "revision": "d8ae3d97d28f01582207ed83e9b297e9",
+    "url": "/ce-admin/static/media/2.d8ae3d97.png"
+  },
+  {
+    "revision": "c0039b5c4da9ef30678938222dc29abb",
+    "url": "/ce-admin/static/media/3.c0039b5c.jpg"
+  },
+  {
+    "revision": "ddde1273d6873992a134c2060351c213",
+    "url": "/ce-admin/static/media/4.ddde1273.png"
+  },
+  {
+    "revision": "9bcdc1bdfb78acc7e7e8f2ff4d1eb2d6",
+    "url": "/ce-admin/static/media/ce.9bcdc1bd.png"
+  },
+  {
+    "revision": "5d8001ad310d6deec77dc32d05514b4c",
+    "url": "/ce-admin/static/media/invictalogo.5d8001ad.png"
+  },
+  {
+    "revision": "1733e4e0d4277a46180e5b5aae8d49f8",
+    "url": "/ce-admin/static/media/logo.1733e4e0.png"
+  },
+  {
+    "revision": "ad6924b9718dae62a110ecf4842578fc",
+    "url": "/ce-admin/static/media/myAthinaLogo.ad6924b9.png"
+  },
+  {
+    "revision": "62c5b7bc988d1208e6c7e17c3251f195",
+    "url": "/ce-admin/static/media/preplogo.62c5b7bc.svg"
+  },
+  {
+    "revision": "3eb614c6e603a184e30b957d37953d0c",
+    "url": "/ce-admin/static/media/tv_logo.3eb614c6.svg"
+  }
+]);
